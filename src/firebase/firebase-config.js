b9beb9c957196c331f0d1ad05285c9a90273101c
Carolina Daza <daza.jeymmy@gmail.com>
2021-01-08 03:45:07 -0500

    280. Crear base de datos de desarrollo

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -12,8 +12,27 @@ import 'firebase/auth';
     messagingSenderId: "567086459647",
     appId: "1:567086459647:web:486867962810f1b6f9baca"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+
+   // Your web app's Firebase configuration testing
+   const firebaseConfigTesting = {
+    apiKey: "AIzaSyBlrFcS-odEEspRalPvIfbBc0QN429z0Hc",
+    authDomain: "react-app-curso-testing.firebaseapp.com",
+    projectId: "react-app-curso-testing",
+    storageBucket: "react-app-curso-testing.appspot.com",
+    messagingSenderId: "977409016266",
+    appId: "1:977409016266:web:3c1c9b7cc715b378904f33"
+  };
+
+  // console.log( process.env); me deja ver el estado de configuracion NODE_ENV: "development"
+  if (process.env.NODE_ENV === 'test') {
+    //testing
+    firebase.initializeApp(firebaseConfigTesting);
+  }else{
+    // dev/prod
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
 
   const db = firebase.firestore(); // esta es la base de datos
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider(); //es para que yo pueda hacer autenticacion con google
